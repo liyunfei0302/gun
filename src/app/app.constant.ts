2542d95d1aced02ccd451180982848bfab2a8c92
Li Yun Fei <liyunfei0514@gmail.com>
2019-03-04 15:46:08 +0800

    添加枪声

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -14,5 +14,23 @@ export const SOUNDS = [
   }, {
     name: 'silencer',
     src: '../../../assets/sound/silencer.mp3',
+  }, {
+    name: 'M16',
+    src: '../../../assets/sound/M16.mp3',
+  }, {
+    name: 'P90',
+    src: '../../../assets/sound/P90.mp3',
+  }, {
+    name: 'P228',
+    src: '../../../assets/sound/P228.mp3',
+  }, {
+    name: 'QBB95',
+    src: '../../../assets/sound/QBB95.mp3',
+  }, {
+    name: '左轮手枪',
+    src: '../../../assets/sound/左轮手枪.mp3',
+  }, {
+    name: '散弹',
+    src: '../../../assets/sound/散弹.mp3',
   },
 ];
